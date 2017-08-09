ef586b9b6e53641cb781e5c7d0e18ff26385702d
Mykola <0xb000@gmail.com>
2017-08-09 02:35:52 +0000

    Add test stub

---
+++ test/robosherlock/TransparentSegmentationTest.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <gtest/gtest.h>
+#include "rs/utils/RSAnalysisEngineManager.h"
+#include "rs/utils/RSAnalysisEngine.h"
+#include <rs/utils/common.h>
+#include <rs/types/all_types.h>
+#include <rs/scene_cas.h>
+
+#include <pcl/point_types.h>
+#include <ros/ros.h>
+
+
+TEST(TransparentSegmentationTest, meshLoadTest)
+{
+  EXPECT_TRUE(true > 0);
+}
