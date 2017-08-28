b994f73092c9bb13d0f9900b423b3823244d9da8
Mykola <0xb000@gmail.com>
2017-08-28 16:26:14 +0000

    Add copyrights, include guards

--- src/utils/include/rs/utils/GeometryCV.h
+++ src/utils/include/rs/utils/GeometryCV.h
@@ -1,8 +1,29 @@
-#pragma once
+/**
+ * Copyright 2017 University of Bremen, Institute for Artificial Intelligence
+ * Author(s): Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
+ *         Thiemo Wiedemeyer <wiedemeyer@cs.uni-bremen.de>
+ *         Jan-Hendrik Worch <jworch@cs.uni-bremen.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __GEOMETRYCV_H__
+#define __GEOMETRYCV_H__
 
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
 #include <pcl/search/kdtree.h>
+
 
 /// \struct PoseRT GeometryCV.h
 /// \brief Rigid 3d transformation presented as rodrigues and translation vector
@@ -668,3 +689,5 @@ namespace GeometryCV {
     return std::tie(sil_to_tmplt_transformation, fitness);
   }
 }
+
+#endif /*__GEOMETRYCV_H__*/
